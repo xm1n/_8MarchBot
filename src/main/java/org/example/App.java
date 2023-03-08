a2c0c1bde773f017e31b1bedc25577d11627c6f6
xm1n <kirillselvonik@gmail.com>
2023-03-08 15:36:09 +0300

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,2 +1,63 @@
-package org.example;public class App {
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import eu.vendeli.tgbot.TelegramBot;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(App.class, args);
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try
+        {
+            telegramBotsApi.registerBot(new Bot());
+            wakeUpDyno();
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+    private static void wakeUpDyno() {
+        try {
+            while(true) {
+
+                URL url = new URL("https://xm2nbot.herokuapp.com/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                int responcode = connection.getResponseCode();
+                System.out.println("RS CODE:"+responcode);
+                if (responcode == HttpURLConnection.HTTP_OK)
+                {
+                    System.out.println("Working");
+                }
+                else{
+                    System.out.println("GET not working");
+                }
+
+                Thread.sleep(60000); //
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
 }
--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,2 +1,74 @@
-package org.example;public class Bot {
+// Defining Package name
+package org.example;
+
+// @Value annotation
+// @Component annotation
+import org.springframework.stereotype.Component;
+// LongPollingBot class
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+// Update Method
+import org.telegram.telegrambots.meta.api.objects.Update;
+// SendMessage method
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+@Component
+class Bot extends TelegramLongPollingBot {
+    String BOT_TOKEN;
+    String BOT_USERNAME;
+
+    String welcomemessage = " с 8 марта!\n\n" + "Желаю тебе счастья, здоровья, успехов во всех твоих начинаниях...\n"
+            + "Пусть в этот день всё будет хорошо :) \u2764";
+
+    Bot() {
+        this.BOT_TOKEN = BOT_TOKEN;
+        this.BOT_USERNAME = BOT_USERNAME;
+    }
+
+    // onUpdateReceived method
+    @Override
+    public void onUpdateReceived(Update update) {
+        // Checking if the update has message and it has text
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Creating object of SendMessage
+            SendMessage message = new SendMessage();
+            // Setting chat id
+            message.setChatId(update.getMessage().getChatId().toString());
+            // Setting reply to message id
+            message.setReplyToMessageId(update.getMessage().getMessageId());
+            // Getting and setting received message text
+            message.setText("Привет, " + update.getMessage().getFrom().getFirstName() + welcomemessage);
+            try {
+                // Sending message
+                execute(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (update.hasMessage() && update.getMessage().getText().equals("")) {
+            // Creating object of SendMessage
+            // Setting chat id
+            message.setChatId(update.getMessage().getChatId().toString());
+            // Setting reply to message id
+            message.setReplyToMessageId(update.getMessage().getMessageId());
+            // Getting and setting received message text
+            message.setText("Бот не увы не умеет читать сообщения :(\n" +
+                    "Если вы хотите что-нибудь написать пишите сюда: @xm1nya_pr\u2764");
+            try {
+                // Sending message
+                execute(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return "xm2nBot";
+    }
+    @Override
+    public String getBotToken() {
+        return "6273105928:AAGRyCPnwEqfOSYnL2x9lRmTHz9hXEkiwDI";
+    }
 }
